56b490238e6d543fbf0013a0b6adc423076a5880
Tony <morrantho@gmail.com>
2017-08-06 14:36:21 -0400

    Finished 1955 API.

---
+++ MEAN/Mongo/1955_api/server.js
@@ -0,0 +1,12 @@
+let express  = require("express");
+let bParse   = require("body-parser");
+let mongoose = require("mongoose");
+let app      = express();
+let port     = 8080;
+app.use(express.static(__dirname + "/client/static"));
+app.use(bParse.urlencoded({ extended: true }));
+app.use(bParse.json());
+app.listen(port);
+
+require("./server/config/mongoose.js");
+require("./server/config/routes.js")(app);
